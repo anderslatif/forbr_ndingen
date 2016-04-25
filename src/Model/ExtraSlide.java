e55ea79042dc9becee4d7e5f62039a4062b766f7
Mikkel Thore Simonsen <m.thore@hotmail.com>
2016-04-25 09:00:30 +0200

    update

---
+++ src/Model/ExtraSlide.java
@@ -0,0 +1,9 @@
+package Model;
+
+/**
+ * Created by Mikkel on 15/04/2016.
+ */
+public class ExtraSlide extends Slide {
+
+
+}
--- src/Model/Slide.java
+++ src/Model/Slide.java
@@ -1,7 +1,17 @@
 package Model;
 
+import javafx.scene.image.Image;
+
+import java.sql.Time;
+
 /**
- * Created by Mikkel on 15/04/2016.
+ * Created by Mikkel on 15/04/2016. Slide superklasse
  */
+
 public class Slide {
+
+    private String slideHeader;
+    private Image slideLogo;
+    private Time duration; // (int hour, int minute, int second)
+
 }
