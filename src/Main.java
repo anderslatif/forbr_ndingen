6e4941d1dc7777eac59599955f0885710d7f4ec4
DRChrist <dennis_christensen87@hotmail.com>
2016-05-24 11:31:29 +0200

    no message

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,9 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception {
 
-<<<<<<< HEAD
-                Layout view = new Layout();
-                Scene scene = new Scene(view.getRootLayout());
-                view.initializeLayout(scene, primaryStage, view);
-
-                //Scene scene = new Scene(view.getRootLayout());
-                //view.initializeLayout(scene, primaryStage);
-=======
+        Layout view = new Layout();
         Scene scene = new Scene(view.getRootLayout());
+
         view.initializeLayout(scene, primaryStage, view);
         double menuPlusTabHeaderHeight = view.getMenuBar().getHeight() + 20;
         double height = Screen.getPrimary().getVisualBounds().getHeight();
@@ -32,35 +26,18 @@ public class Main extends Application {
         primaryStage.setMinHeight(height);
         primaryStage.setMinWidth(width);
         primaryStage.setResizable(false);
->>>>>>> 6c25b465b312c8ea291fce3d2521e60529cbac12
 
-                //Scene scene = new Scene(view.getRootLayout());
-                //view.initializeLayout(scene, primaryStage, view);
-                double menuPlusTabHeaderHeight = view.getMenuBar().getHeight() + 20;
-                double height = Screen.getPrimary().getVisualBounds().getHeight();
-                double width = (Screen.getPrimary().getVisualBounds().getHeight() - menuPlusTabHeaderHeight) / 1.7777;
-                primaryStage.setMinHeight(height);
-                primaryStage.setMinWidth(width);
-            }
-
-<<<<<<< HEAD
-            scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
-
-
-            primaryStage.getIcons().add(new Image("file:src/logo.png"));
-=======
         scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+
+
         primaryStage.getIcons().add(new Image("file:src/logo.png"));
->>>>>>> 6c25b465b312c8ea291fce3d2521e60529cbac12
+    }
+
 
 /*        primaryStage.setOnCloseRequest( e -> {
             e.consume();
             view.savePresentationBeforeClosingAll();
         });*/
-
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        }
 
 
 
